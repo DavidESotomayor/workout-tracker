8c762ceffc51171a0d7c768534b350434fe6dfdd
David Sotomayor <david.elijah.sotomayor@gmail.com>
2021-03-16 04:13:46 -0500

    setting up server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}!`);
+});
