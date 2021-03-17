fc404c575963ced882416867d18324c2baa7c035
David Sotomayor <david.elijah.sotomayor@gmail.com>
2021-03-17 02:16:42 -0500

    create a route for the workout range

--- routes/api.js
+++ routes/api.js
@@ -41,4 +41,19 @@ router.put('/api/workouts/:id', (req, res) => {
         });
 });
 
+// get workouts in range
+router.get("/api/workouts/range", (req, res) => {
+    db.Workout.aggregate([
+      { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+      { $addFields: { weight: { $sum: "$exercises.weight" } } },
+    ])
+      .limit(7)
+      .then((workout) => {
+        res.json(workout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
 module.exports = router;
