015a65e3fedb85e0f51aade87bfca249f14ec7db
David Sotomayor <david.elijah.sotomayor@gmail.com>
2021-03-16 23:14:00 -0500

    create routes

--- routes/api.js
+++ routes/api.js
@@ -3,17 +3,40 @@ const router = require('express').Router()
 
 // get workouts
 router.get('/api/workouts', (req, res) => {
-    db.Workout.find({}).then(workout => {
-        workout.forEach(exercise => {
+    db.Workout.find({}).then(response => {
+        response.forEach(workout => {
             let total = 0;
-            exercise.exercises.forEach(result => {
+            workout.exercises.forEach(result => {
                 total += result.duration;
             });
-            exercise.totalDuration = total;
+            workout.totalDuration = total;
         });
-        res.json(workout);
+        res.json(response);
     }).catch( err => {
         res.json(err);
     });
 });
 
+//create workout
+router.post('/api/workouts', (req, res) => {
+    db.Workout.create(req.body).then((workout => {
+        res.json(workout);
+    })).catch(err => {
+        res.json(err);
+    });
+});
+
+// add exercise
+router.put('/api/workouts/:id', (req, res) => {
+    db.Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            $inc: { totalDuration: req.body.duration },
+            $push: { exercises: req.body }
+        },
+        { new: true }).then(workout => {
+            res.json(workout);
+        }).catch(err => {
+            res.json(err);
+        });
+});
